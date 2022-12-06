6edfd4990c28fe785f987885fb980c3f253b41bc
Brian Park <park-brian@users.noreply.github.com>
2022-12-06 11:11:15 -0500

    update entrypoints for env config

--- database/createDatabase.js
+++ database/createDatabase.js
@@ -1,19 +1,20 @@
 import { fileURLToPath } from "url";
 import { createRequire } from "module";
 import minimist from "minimist";
+import { config } from "dotenv";
 import { createPostgresClient } from "./services/utils.js";
 import { createSchema } from "./services/pipeline.js";
 
 // determine if this script was launched from the command line
 const isMainModule = process.argv[1] === fileURLToPath(import.meta.url);
 const require = createRequire(import.meta.url);
+config();
 
 if (isMainModule) {
-  const config = require("./config.json");
   const args = minimist(process.argv.slice(2));
   const schema = await require(args.schema || "./schema.json");
 
-  const connection = await createPostgresClient(config.database);
+  const connection = await createPostgresClient(process.env);
   await createSchema({ connection, schema });
   console.log("Initialized all tables");
   process.exit(0);
--- database/importDatabase.js
+++ database/importDatabase.js
@@ -3,7 +3,8 @@ import { runTask } from "./services/pipeline.js";
 import { fileURLToPath } from "url";
 import { createRequire } from "module";
 import minimist from "minimist";
-import { loadAwsCredentials, withDuration } from "./services/utils.js";
+import { config } from "dotenv";
+import { withDuration } from "./services/utils.js";
 import { LocalProvider } from "./services/providers/localProvider.js";
 import { S3Provider } from "./services/providers/s3Provider.js";
 import { getLogger } from "./services/logger.js";
@@ -12,17 +13,16 @@ import { S3Client } from "@aws-sdk/client-s3";
 // determine if this script was launched from the command line
 const isMainModule = process.argv[1] === fileURLToPath(import.meta.url);
 const require = createRequire(import.meta.url);
+config();
 
 if (isMainModule) {
   const args = minimist(process.argv.slice(2));
-  const { aws, database } = require("./config.json");
   const schema = require(args.schema || "./schema.json");
   const sources = require(args.sources || "./samples.json");
   const providerName = args.provider || "local";
   const providerArgs = [...args._];
-  loadAwsCredentials(aws);
 
-  const connection = await createPostgresClient(database);
+  const connection = await createPostgresClient(process.env);
   const sourceProvider = getSourceProvider(providerName, providerArgs);
   const logger = getLogger("import");
   await importDatabase(connection, schema, sources, sourceProvider, logger);
