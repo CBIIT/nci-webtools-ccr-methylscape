f8b0c097b09e647a355b02ac061705994003092c
Brian Park <park-brian@users.noreply.github.com>
2023-01-04 12:01:04 -0500

    adjust position of table group controls

--- client/src/modules/analysis/table/table-group-controls.js
+++ client/src/modules/analysis/table/table-group-controls.js
@@ -23,18 +23,20 @@ export default function GroupTabs() {
   }
 
   return (
-    <div>
-      <Button size="sm" variant="success" onClick={addTab}>
-        + Group
-      </Button>
+    <div className="d-flex justify-content-between">
+      <div>
+        <Button size="sm" variant="success" onClick={addTab}>
+          + Group
+        </Button>
+        {+umapPoints.selectedGroup !== 0 && (
+          <Button size="sm" variant="danger" onClick={() => removeTab(umapPoints.selectedGroup)} className="ms-3">
+            - Group
+          </Button>
+        )}
+      </div>
       <Button size="sm" variant="outline-danger" onClick={resetPoints} className="ms-3">
         Reset
       </Button>
-      {+umapPoints.selectedGroup !== 0 && (
-        <Button size="sm" variant="danger" onClick={() => removeTab(umapPoints.selectedGroup)} className="ms-3">
-          - Group
-        </Button>
-      )}
     </div>
   );
 }
