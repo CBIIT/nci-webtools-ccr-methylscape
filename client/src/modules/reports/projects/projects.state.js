2ff4f34fdbcf857d3766ef95eda94be618e42c7d
Brian Park <park-brian@users.noreply.github.com>
2023-01-19 21:41:12 -0500

    simplify selectors

--- client/src/modules/reports/projects/projects.state.js
+++ client/src/modules/reports/projects/projects.state.js
@@ -1,5 +1,4 @@
 import { atom, selector } from "recoil";
-import groupBy from "lodash/groupBy.js";
 import { methylscapeData } from "../data.state";
 
 export const defaultProjectState = {
@@ -15,9 +14,7 @@ export const projectsTableData = selector({
   key: "projectsTableData",
   get: ({ get }) => {
     const { data } = get(methylscapeData);
-
     if (!data.length) return [];
-
     return Object.values(data);
   },
 });
@@ -31,45 +28,10 @@ export const selectedRow = selector({
   key: "selectedProject",
   get: ({ get }) => {
     const { selectedProject } = get(projectState);
-    if (!selectedProject) return false;
-
-    let { sampleData } = get(methylscapeData);
-    let data = sampleData.filter(({ unifiedSamplePlate }) => unifiedSamplePlate == selectedProject);
-
-    const getGroups = (key) => Object.entries(groupBy(data, key)).map((group) => [group[0], group[1].length]);
-
-    const getGender = () => {
-      let cur = {};
-      let pieData = [];
-      data.forEach((row) => {
-        cur[row.sex] = cur[row.sex] + 1 || 1;
-      });
-      Object.keys(cur).forEach((k) => {
-        pieData.push([k, cur[k]]);
-        //pieData[0].push(k);
-        //pieData[1].push(cur[k]);
-      });
-      return pieData;
-    };
-
-    const getAgeDistribution = () => {
-      let cur = {};
-      let pieData = [];
-      data.forEach((row) => {
-        cur[row.age] = cur[row.age] + 1 || 1;
-      });
-      Object.keys(cur).forEach((k) => {
-        pieData.push([k, cur[k]]);
-      });
-      return pieData;
-    };
-
-    const methylationClasses = getGroups("CNSv12b6_subclass1");
-    const gender = getGender();
-    const ageDistribution = getAgeDistribution();
-
-    // console.log({ methylationClasses, gender });
-
-    return { selectedProject, methylationClasses, gender, ageDistribution };
+    const { sampleData } = get(methylscapeData);
+    const data = sampleData.filter(
+      ({ unifiedSamplePlate }) => !selectedProject || unifiedSamplePlate === selectedProject
+    );
+    return { selectedProject, data };
   },
 });
