acb57b7fab42b3e421037e54cbfb146d1b057eb0
Phyllip Cho <phyllip3@users.noreply.github.com>
2023-08-07 14:31:07 -0400

    submissions details placeholder

---
+++ client/src/modules/submissions/submissions-details.js
@@ -0,0 +1,59 @@
+import { Row, Col, Card, Form, Button } from "react-bootstrap";
+import Table from "../components/table";
+
+export default function SubmissionsDetails({}) {
+  const columns = [
+    { accessor: "1", Header: "Sample Name" },
+    { accessor: "2", Header: "Project" },
+    { accessor: "2", Header: "Experiment" },
+    { accessor: "2", Header: "Sample Date" },
+    { accessor: "2", Header: "Sex" },
+    { accessor: "2", Header: "Age" },
+    { accessor: "2", Header: "Provided Diagnosis" },
+  ];
+
+  return (
+    <div>
+      <Card className="bg-white">
+        <Card.Body>
+          <Row>
+            <Col sm="4" className="d-flex justify-content-between">
+              <Form.Label>Submission Name</Form.Label>
+              <div>name</div>
+            </Col>
+            <Col sm="4" className="d-flex justify-content-between">
+              <Form.Label>Submitter</Form.Label>
+              <div>name</div>
+            </Col>
+            <Col sm="4" className="d-flex justify-content-between">
+              <Form.Label>Investigator</Form.Label>
+              <div>name</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="4" className="d-flex justify-content-between">
+              <Form.Label>Organization</Form.Label>
+              <div>name</div>
+            </Col>
+            <Col sm="4" className="d-flex justify-content-between">
+              <Form.Label>Project</Form.Label>
+              <div>name</div>
+            </Col>
+            <Col sm="4" className="d-flex justify-content-between">
+              <Form.Label>Experiment</Form.Label>
+              <div>name</div>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+
+      <Card className="bg-white mt-3">
+        <Card.Body>
+          <div>placeholder</div>
+          {/* <Table name="" data={[{ 1: "test" }]} columns={columns} options={{}} /> */}
+          <Button variant="link">Back to Submissions List</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
