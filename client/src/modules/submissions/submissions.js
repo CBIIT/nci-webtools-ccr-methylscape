1422c6693b41ac24231bc9e49394c24415d45e1b
Phyllip Cho <phyllip3@users.noreply.github.com>
2023-08-17 11:45:48 -0400

    add loading and error overlay

--- client/src/modules/submissions/submissions.js
+++ client/src/modules/submissions/submissions.js
@@ -1,5 +1,8 @@
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Alert } from "react-bootstrap";
+import ErrorBoundary from "../components/error-boundary";
+import Loader from "../components/loader";
 
 export default function Submissions() {
   const location = useLocation().pathname.split("/").slice(-1);
@@ -12,7 +15,17 @@ export default function Submissions() {
 
   return (
     <div className="d-flex justify-content-center my-3">
-      <Outlet />
+      <ErrorBoundary
+        fallback={
+          <Alert variant="danger">
+            An internal error prevented data from loading. Please contact the website administrator if this problem
+            persists.
+          </Alert>
+        }>
+        <Suspense fallback={<Loader message="Loading" />}>
+          <Outlet />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
