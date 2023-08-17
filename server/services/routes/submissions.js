2cc74264d73139ef9e48afa2ff7942ebf0701284
Phyllip Cho <phyllip3@users.noreply.github.com>
2023-08-17 11:45:31 -0400

    add userSamples route

--- server/services/routes/submissions.js
+++ server/services/routes/submissions.js
@@ -43,10 +43,9 @@ router.post(
   async (request, response) => {
     const { connection } = request.app.locals;
     const { submission, metadata, ...rest } = JSON.parse(request.body.data);
-    const { uuid } = request.params;
 
-    const submissionsId = await connection("submissions").insert(submission).returning("id")[0].id;
-    const userSamplesId = metadata.forEach(
+    const submissionsId = (await connection("submissions").insert(submission).returning("id"))[0].id;
+    const userSamplesId = metadata.map(
       async (sample) =>
         await connection("userSamples")
           .insert({ submissionsId, ...sample })
@@ -56,5 +55,26 @@ router.post(
     response.json({ submissionsId, userSamplesId });
   }
 );
+
+router.get("/userSamples/:submissionsId", requiresRouteAccessPolicy("AccessApi"), async (request, response) => {
+  const { connection } = request.app.locals;
+  const { submissionsId } = request.params;
+
+  const results = await connection("userSamples")
+    .leftJoin("submissions", "userSamples.submissionsId", "submissions.id")
+    .leftJoin("user", "submissions.userId", "user.id")
+    .leftJoin("organization", "submissions.organizationId", "organization.id")
+    .select(
+      "userSamples.*",
+      "submissions.submissionName",
+      "submissions.experiment",
+      "submissions.investigator",
+      "organization.name as organizationName",
+      connection.raw(`"user"."firstName" || ' ' || "user"."lastName" as submitter`)
+    )
+    .where("submissionsId", submissionsId);
+
+  response.json(results);
+});
 
 export default router;
