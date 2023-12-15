10153412dbeca62f27d51e77650a17e05aa9f0f0
Phyllip Cho <phyllip3@users.noreply.github.com>
2023-12-15 16:13:07 -0500

    read excel from buffer instead of stream

--- client/src/modules/submissions/submissions.utils.js
+++ client/src/modules/submissions/submissions.utils.js
@@ -181,6 +181,6 @@ export function sampleFilePairs(files) {
 
 export async function parseExcelToCsv(file) {
   const workbook = new Excel.Workbook();
-  await workbook.xlsx.read(file.stream(), { rawNumbers: true });
+  await workbook.xlsx.load(await file.arrayBuffer());
   return (await workbook.csv.writeBuffer()).toString();
 }
